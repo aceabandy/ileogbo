76515465eaf6483c5692b46361e7402f85a79285
aceabandy <aceabandi@gmail.com>
2024-10-05 23:36:26 -0700

    settings.py

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5sas4)%j8y+v@tb$^7f_&s*5_6nrk#cq*e0#44x6^20oqg!+=)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ileogbo.vercel.app']
 
 
 # Application definition
